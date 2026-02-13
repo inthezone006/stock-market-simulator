b2769f7bbf1f33928822ba042e7c93844780a8a8
Rahul Menon <rahul.menon85280@gmail.com>
2026-02-13 16:51:23 -0500

    Added version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.rahul.stocksim"
         minSdk = 24
         targetSdk = 35
-        versionCode = 20
-        versionName = "2.9"
+        versionCode = 21
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
